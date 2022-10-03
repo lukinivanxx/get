70385f403fe16251260f639c270ffa32827d48fe
Иван Лукин <xrust65@gmail.com>
2022-10-04 01:26:02 +0300

    Kinda done?

--- Laba/task3.py
+++ Laba/task3.py
@@ -3,10 +3,10 @@ try:
     pwm = 22; f = 100
     gp.setmode(gp.BCM)
     gp.setup(pwm, gp.OUT)
-    pwmOut = gp.PWM(pwm, f)
+    pwmOut = gp.PWM(pwm, f)  # pwm - номер выхода, f - частота мерцания. 
     pwmOut.start(0)
     while True:
         dcycle = int(input())
-        pwmOut.ChangeDutyCycle(dcycle)
+        pwmOut.ChangeDutyCycle(dcycle)  #изменяем duty cycle, то есть меняем то, насколько долго будет гореть диод в 1 периоде (меняем яркость при достаточной частоте)
     finally:
         gp.cleanup()
